b262515b6ec03370312e9caef3d627f69d639377
Dhanashree1991 <dhnpatil01@gmail.com>
2019-01-28 12:35:00 +0530

    commit

---
+++ JSON_Proj/src/main/java/JSON_Proj/JSON_Proj/App.java
@@ -0,0 +1,13 @@
+package JSON_Proj.JSON_Proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
